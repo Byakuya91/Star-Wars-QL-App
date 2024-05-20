afd6d289c231466e026841e94d323d6e965bea22
Byakuya91 <nganti91@yahoo.com>
2024-05-20 14:40:53 -0500

    I have successfully resolved the problem of being able to filter based off fields and I even added a means to clear the search via a button. I will be working on being able to sort the data based on a drop-down and fields and filter based on the sort, alongside a theme and dark and light mode.

--- src/Components/CharacterTable/CharacterTable.jsx
+++ src/Components/CharacterTable/CharacterTable.jsx
@@ -6,11 +6,9 @@ import { useQuery } from '@apollo/client';
 // ?CSS imports 
 import './CharacterTable.css';
 
-
 // ?Query imports 
 import { GET_STAR_WARS_CHARACTERS } from '../Querries/StarWarsNames';
 import SearchBar from '../StarWarsSearchBar/SearchBar';
-
 
 // TODOS:KEY TASKS
 // 1) Refactor query calls to include Home-world and species(DONE)
@@ -20,102 +18,41 @@ import SearchBar from '../StarWarsSearchBar/SearchBar';
 // TODOS: supplementary tasks
 // 1) Theme and Dark theme toggler 
 
-
-
 const CharacterTable = () => {
-
-// ? Pieces of states
-// ? Toggling the table view
-const [showTable, setShowTable] = useState(false);
-// ? SearchBar results state
-const [searchTerm, setSearchTerm] = useState('');
+//  ? Defining keys based on the fields for the table
+const Star_keys = ['name', 'species.name', 'homeworld.name'];
 
 
-  //! Fetch data for Star Wars names 
-  const { loading: charactersLoading, error: charactersError, data:charactersData } = useQuery(GET_STAR_WARS_CHARACTERS, {
-    skip: !showTable // Skip the query if showTable is false
-  });
+    // ? Pieces of states
+    // ? Toggling the table view
+    const [showTable, setShowTable] = useState(false);
+    // ? SearchBar results state
+    const [searchTerm, setSearchTerm] = useState('');
 
-// ! Fetch Data for Star Wars Species and HomeWorld ( NOT THE RIGHT QUERY)
-  // const { loading: speciesLoading, error: speciesError, data:speciesData } = useQuery(GET_STAR_WARS_SPECIES_AND_WORLDS, {
-  //   skip: !showTable // Skip the query if showTable is false
-  // });
+    //! Fetch data for Star Wars names 
+    const { loading: charactersLoading, error: charactersError, data: charactersData } = useQuery(GET_STAR_WARS_CHARACTERS, {
+        skip: !showTable // Skip the query if showTable is false
+    });
 
+    // ? Handler functions
+    // ? Showing and hiding the table
+    const handleShowTableClick = () => {
+        setShowTable(true); // Set showTable to true when the button is clicked
+    };
 
-  
-  
+    const handleHideTableClick = () => {
+        setShowTable(false); // Set showTable to false when the "Hide" button is clicked
+    };
 
-  // ? Handler functions
+    // ? Changing the search term 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
 
-  // ? Showing and hiding the table
-  const handleShowTableClick = () => {
-    setShowTable(true); // Set showTable to true when the button is clicked
-  };
-
-  const handleHideTableClick = () => {
-    setShowTable(false); // Set showTable to false when the "Hide" button is clicked
-  };
-
-  // ? Changing the search term 
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  // ! Create a function to handle the filtering of the table
-
-  // ? STEP ONE: create an array to be used as keys for the fields
-  const Star_Keys = ['name', 'species', 'homeworld'];
-
-// ?Testing the keys 
-// console.log(object);
-
-//TODO: Create a search function for the filtered results 
-const starSearch = (data) =>{
-  return data.filter(item =>
-  //  ? For each item, check if one of the Star_Keys contains the search term
-  Star_Keys.some((Star_key) => {
-  //  ? Split the keys to handle nested properties
-  const Star_Split_keys = Star_key.split('.');
-// ? Start with the current item
-  let  Star_value = item;
-// ? traverse the nested properties
- Star_Split_keys.forEach(k => {
-    // ? Update the 'value' to the nested property value or null/undefined if it does NOT exist.
-    Star_value = Star_value && Star_value[k];
- });
-  //  ? Check if the final value exists  and contains the searchTerm
-  return typeof Star_value === 'string' && Star_value.toLowerCase().includes(searchTerm.toLowerCase());
-
-  })
-
-  );
-};
-
-
-
-
-  // OLD CODE 
-  // if (!showTable) {
-  //   return (
-  //     <div>
-  //       <button onClick={handleShowTableClick}>Show Star Wars Characters</button>
-  //     </div>
-  //   );
-  // }
-
-  // Trying to make a tenary operation for the code
-//  showTable ? 
-
-  // ? In case the data does NOT load for Names, Species and Homeworld
-
-  if (charactersLoading  ) 
-    return <p>Loading...</p>;
-
-  if (charactersError) 
-    return <p>Error: {charactersError.message} </p>;
-
-//!  Create the filtered data that will be used by the new Map
-const filteredStarWarsCharacters = charactersData && charactersData.allPeople && charactersData.allPeople.people ? starSearch(charactersData.allPeople.people) : [];
+    // ? Clear the searchBar
+    const clearSearchBar = () =>{
+      setSearchTerm('');
+    }
 
 // USING MOCK DATA to fill in the table and get a sense of what it looks like
 // const mockData = {
@@ -133,48 +70,86 @@ const filteredStarWarsCharacters = charactersData && charactersData.allPeople &&
 //     ]
 //   }
 // };
-// TODO: Write the units test using JEST framework, mock the GraphQL data.Mock GraphQL response locally
-// TODO: Use it in the unit test cases. GraphQL proper response, Case one.
 
-return (
-  <div>
-    {showTable ? (
-      <div className='container'>
-       <SearchBar searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
-        <button  className = "btn" 
-        onClick={handleHideTableClick}>Hide Star Wars Characters</button>
-        <table>
-          <thead>
-            <tr>
-            <th>No.</th>
-              <th>Name</th>
-              <th>Species</th>
-              <th>Homeworld</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Map over the fetched data and display names */}
 
-            {filteredStarWarsCharacters.map((character, index) => (
-              <tr key={index}>
-                  <td> <b>{index + 1}</b></td> {/* Display the index starting from 1 */}
-                <td> <b>{character.name}</b></td>
-                <td><b>{character.species ? character.species.name : 'Unknown'}</b></td>
-                <td><b>{character.homeworld ? character.homeworld.name : 'Unknown'}</b></td>
-              </tr>
-            ))}
-            {/* <td>{speciesData.allPeople.people}</td> */}
-          </tbody>
-        </table>
-      </div>
-    ) : (
-      <div>
-        <button onClick={handleShowTableClick}>Show Star Wars Characters</button>
-      </div>
-    )}
-  </div>
-);
 
+
+    // TODO: Create a search function for the filtered results 
+    const starSearch = (data) => {
+        return data.filter(item =>
+            //  ? For each item, check if one of the Star_Keys contains the search term
+            Star_keys.some((key) => {
+                // Split the keys to handle nested properties
+                const keys = key.split('.');
+                // Start with the current item
+                let value = item;
+                // Traverse the nested properties
+                keys.forEach(k => {
+                    value = value && value[k];
+                });
+                // Check if the final value exists and contains the searchTerm
+                return typeof value === 'string' && value.toLowerCase().includes(searchTerm.toLowerCase());
+            })
+        );
+    };
+
+
+    // OLD CODE 
+  // if (!showTable) {
+  //   return (
+  //     <div>
+  //       <button onClick={handleShowTableClick}>Show Star Wars Characters</button>
+  //     </div>
+  //   );
+  // }
+
+  // Trying to make a tenary operation for the code
+//  showTable ? 
+
+    // ? In case the data does NOT load for Names, Species and Homeworld
+    if (charactersLoading) 
+        return <p>Loading...</p>;
+    
+    if (charactersError) 
+        return <p>Error: {charactersError.message} </p>;
+    
+    //!  Create the filtered data that will be used by the new Map
+    const filteredStarWarsCharacters = charactersData && charactersData.allPeople && charactersData.allPeople.people ? starSearch(charactersData.allPeople.people) : [];
+
+    return (
+        <div>
+            {showTable ? (
+                <div className='container'>
+                    <SearchBar searchTerm={searchTerm} handleSearchChange={handleSearchChange}  clearSearchBar = {clearSearchBar}/>
+                    <button className="btn" onClick={handleHideTableClick}>Hide Star Wars Characters</button>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Species</th>
+                                <th>Homeworld</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {filteredStarWarsCharacters.map((character, index) => (
+                                <tr key={index}>
+                                    <td><b>{index + 1}</b></td> {/* Display the index starting from 1 */}
+                                    <td><b>{character.name}</b></td>
+                                    <td><b>{character.species ? character.species.name : 'Unknown'}</b></td>
+                                    <td><b>{character.homeworld ? character.homeworld.name : 'Unknown'}</b></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            ) : (
+                <div>
+                    <button onClick={handleShowTableClick}>Show Star Wars Characters</button>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default CharacterTable;
--- src/Components/StarWarsSearchBar/SearchBar.jsx
+++ src/Components/StarWarsSearchBar/SearchBar.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+// ? CSS imports
+import "../StarWarsSearchBar/SearchBar.css"
 
-const SearchBar = ({ searchTerm, handleSearchChange }) => {
+const SearchBar = ({ searchTerm, handleSearchChange, clearSearchBar }) => {
     console.log("The Search is: ", searchTerm);
   return (
-    <input
-      type='text'
-      placeholder='Search...'
-      value={searchTerm}
-      onChange={handleSearchChange}
-      className='search-input'
-    />
+    <div className='search-bar'>
+      <input
+        type='text'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className='search-input'
+      />
+       {searchTerm && (
+        <button className="clear-button" onClick={clearSearchBar}>
+          &times;
+        </button>
+      )}
+    </div>
+    
   );
 };
 
