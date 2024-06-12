bcf452c7943396bf0359dc7e5631168e41a1e910
Byakuya91 <nganti91@yahoo.com>
2024-06-12 14:26:09 -0500

    I've just updated my TODO list in Index on the tasks. I am currently on figuring out how to fetch the SWAPI data into my server so as to test.

--- Star-Wars-QL-Backend/index.js
+++ Star-Wars-QL-Backend/index.js
@@ -6,14 +6,14 @@ const typeDefs = require("./Schemas/characterSchema");
 const resolvers = require("./resolvers/resolvers.js");
 
 // TODO list:
-// 1) Setup the server using Apollo server and test it(ONGOING).
-// 2) Create the mock data(ONGOING).
-// 3) Create and define Schemas(ONGOING).
-// 4) Test the server by making some GraphQL calls.
-// 5) Setup resolvers and get them to work.
-// 6) Figure out the CRUD operations and mutations.
-// 7) Test the resolvers and mutations with the mock data.
-// 8) Integrate the SWAPI data into the server.
+// 1) Setup the server using Apollo server and test it(DONE).
+// 2) Create the mock data(DONE).
+// 3) Create and define Schemas(DONE).
+// 4) Test the server by making some GraphQL calls(DONE).
+// 5) Setup resolvers and get them to work(DONE).
+// 6) Figure out the CRUD operations and mutations(DONE).
+// 7) Test the resolvers and mutations with the mock data(DONE).
+// 8) Integrate the SWAPI data into the server(ONGOING).
 // 9) Connect the server to the client/ front-end of the application and correct for bugs.
 
 //STEP ONE:  Create Apollo Server
