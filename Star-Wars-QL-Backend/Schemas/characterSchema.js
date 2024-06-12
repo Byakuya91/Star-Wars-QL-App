dbc6233d82e1c6ea45087bc5f6a74d4070be8cfe
Byakuya91 <nganti91@yahoo.com>
2024-06-12 12:02:29 -0500

    I have coded out the ADD CRUD functionality to the data. I'm moving onto the delete portion of the data.

--- Star-Wars-QL-Backend/Schemas/characterSchema.js
+++ Star-Wars-QL-Backend/Schemas/characterSchema.js
@@ -1,5 +1,5 @@
 // TODO: Define the blueprint and schemas for SWAPI data
-// !1 Need to define mutations but what matters is displaying the mock data first and foremost
+// !1 Need to define mutations but what matters is displaying the mock data first and foremost(WORKING)
 
 const { gql } = require("apollo-server");
 
@@ -14,6 +14,13 @@ const typeDefs = gql`
 
   type Query {
     allPeople: [Character]
+  }
+
+  type Mutation {
+    addCharacter(name: String!, species: String, homeworld: String): Character
+    #! IMPORTANT: need to code out the delete and update FIRST
+    # deleteCharacter(id: ID!): Character
+    # updateCharacter(id: ID!, name: String, species: String, homeworld: String): Character
   }
 `;
 
--- Star-Wars-QL-Backend/resolvers/resolvers.js
+++ Star-Wars-QL-Backend/resolvers/resolvers.js
@@ -1,10 +1,31 @@
-// TODO: Setup the resolvers to display the data I have.
+// TODO: Setup the resolvers to display the data I have(DONE)
+// 1) Set up resolver to ADD a Star Wars character name, species and homeworld.
+// 2) Set up resolver to DELETE a Star Wars character name, species and homeworld.
+// 3) Set up resolver to EDIT a Star Wars character name, species and homeworld.
 
+// ?OTHER FILE IMPORTS
+const { v4: uuidv4 } = require("uuid");
+
+// ? MOCK DATA IMPORT
 const characters = require("../data/characters");
 
 const resolvers = {
   Query: {
     allPeople: () => characters,
+  },
+  Mutation: {
+    //TODO: ADD a new character(full name, species and homeworld)
+    // STEP ONE: define the resolver inside the mutation
+    addCharacter: (_, { name, species, homeworld }) => {
+      // Generate a unique ID for the new character
+      const newCharacter = { id: uuidv4(), name, species, homeworld };
+
+      // Add the new character to the characters list
+      characters.push(newCharacter);
+
+      // Return the newly added character
+      return newCharacter;
+    },
   },
 };
 
