f26ad1d2847ef5f4ed5c459520dc13a369a20e5e
Byakuya91 <nganti91@yahoo.com>
2024-08-08 15:01:04 -0400

    I have resumed work on unit testing the character table. The tests for testing the filter were successful,but the sorting tests are proving to be much harder due to how the rows of the table do not just include the fields, like name, but buttons and actions. I am going to try utilizing 'within' from react testing library to see if that will resolve the issue.I had already utilizes a regex pattern to see if that would help but it partially helped.

--- Star-Wars-QL-Frontend/src/Components/CharacterTable/CharacterTable.test.jsx
+++ Star-Wars-QL-Frontend/src/Components/CharacterTable/CharacterTable.test.jsx
@@ -2,7 +2,13 @@
 import React from "react";
 
 // ?Testing libraries
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  fireEvent,
+  within,
+} from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import "@testing-library/jest-dom"; // Correct import for jest-dom
 
@@ -14,6 +20,15 @@ import CharacterTable from "./CharacterTable";
 
 // ?Query imports
 import { GET_STAR_WARS_CHARACTERS } from "../Querries/StarWarsNames";
+
+// TODO: Tests I wish to cover
+// 1 Renders the table from the API (DONE)
+// 2 Does NOT render the table from the API(DONE)
+// 3 Search functionality(DONE)
+// 4 Sorting functionality(ONGOING)
+// 5 pagimation
+//  6 Add character functionality
+// 7 Delete character functionality
 
 // ? STEP ONE: Mock the data
 // Mock data for Apollo Client
@@ -102,5 +117,60 @@ describe("CharacterTables Component", () => {
         screen.getByText("Error: Failed to fetch data")
       ).toBeInTheDocument();
     });
+  });
+  //? Search functionality
+  it("filters the data based on a search term.", async () => {
+    // Render the character table
+    render(
+      <MockedProvider mocks={[starWarsMockData[0]]} addTypename={false}>
+        <CharacterTable />
+      </MockedProvider>
+    );
+    fireEvent.click(screen.getByText("Show Star Wars Characters"));
+
+    // Waiting for the table to be rendered
+
+    await waitFor(() => {
+      expect(screen.getByText("Luke Skywalker")).toBeInTheDocument();
+    });
+
+    // Enter the Search term
+    fireEvent.change(screen.getByPlaceholderText("Search..."), {
+      target: { value: "Leia" },
+    });
+
+    // Wait for the table to be updated
+    await waitFor(() => {
+      expect(screen.getByText("Leia Organa")).toBeInTheDocument();
+      expect(screen.queryByText("Luke Skywalker")).not.toBeInTheDocument();
+    });
+  });
+
+  // ? Sort test: testing if the table can be sorted by either "Name", "Species" or "Homeworld"
+  it("sorts the data by name when 'Name' header is clicked", async () => {
+    //  STEP ONE: Wait for the table to be rendered
+    render(
+      <MockedProvider mocks={[starWarsMockData[0]]} addTypename={false}>
+        <CharacterTable />
+      </MockedProvider>
+    );
+    fireEvent.click(screen.getByText("Show Star Wars Characters"));
+
+    await waitFor(() => {
+      expect(screen.getByText("Luke Skywalker")).toBeInTheDocument();
+    });
+
+    // STEP TWO: simulate the click of "Name" header to sort by "Name".
+    fireEvent.click(screen.getByText(/^Name/));
+
+    // STEP THREE: create the rows for the simulation
+    const rows = screen.getAllByRole("row");
+
+    expect(rows[1]).toHaveTextContent("C-3PO");
+    expect(rows[2]).toHaveTextContent("Darth Vader");
+    expect(rows[3]).toHaveTextContent("Han Solo");
+    expect(rows[4]).toHaveTextContent("Leia Organa");
+    expect(rows[5]).toHaveTextContent("Luke Skywalker");
+    expect(rows[6]).toHaveTextContent("R2-D2");
   });
 });
